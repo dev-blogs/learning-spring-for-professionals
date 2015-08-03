a31418ca0569400f5d779cf22c18263e0df13892
zheka <zheka0783@yandex.ru>
2015-08-03 13:10:49 +0300

    created ch10_02_spring_data_jpa project

---
+++ ch10_02_spring_data_jpa/src/main/java/com/dev/blogs/ch10_02_spring_data_jpa/App.java
@@ -0,0 +1,13 @@
+package com.dev.blogs.ch10_02_spring_data_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
