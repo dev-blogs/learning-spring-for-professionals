9cf73589adfc1685c04494aa8b5147660ecee0c7
zheka <zheka0783@yandex.ru>
2015-08-04 18:22:08 +0300

    created project ch10_02_audit

---
+++ ch10_02_audit/src/main/java/com/dev/blogs/ch10_02_audit/App.java
@@ -0,0 +1,13 @@
+package com.dev.blogs.ch10_02_audit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
