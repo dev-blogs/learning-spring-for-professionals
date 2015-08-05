08e7dcc53b860a02d5700d4b4a6553cf3058ce6b
zheka <zheka0783@yandex.ru>
2015-08-05 12:16:33 +0300

    added project ch10_04_hibernate-envers

---
+++ ch10_04_hibernate-envers/src/main/java/com/dev/blogs/ch10_04_hibernate_envers/App.java
@@ -0,0 +1,13 @@
+package com.dev.blogs.ch10_04_hibernate_envers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
